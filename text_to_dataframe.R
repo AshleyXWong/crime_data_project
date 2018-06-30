df85bd03625dbbea2af6bb24658f2323270542a8
Ashley Wong <ashleywong@Ashleys-MacBook-Pro.local>
2018-06-30 11:26:56 -0400

    script that converts text document we got from dataset to dataframe for datamanipulation

---
+++ text_to_dataframe.R
@@ -0,0 +1,11 @@
+crime_data<-read.csv("crimedata.txt")
+
+vector_attributes <- vector('character')
+attributes_list <- readLines("attributes.txt")
+for(i in 1:length(attributes_list)){
+  split <- unlist(strsplit(attributes_list[i], split=" "))
+  vector_attributes <- c(split[2], vector_attributes)
+}
+vector_attributes <- rev(vector_attributes)
+
+colnames(crime_data) <- vector_attributes
